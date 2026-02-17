c692efe709c3a164b18154462fb2b5d7f59dd923
AmiriteZ <ar.seraj03@gmail.com>
2026-02-17 19:14:03 +0000

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -7,7 +7,9 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set in environment variables");
 }
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const pool = new Pool({
   connectionString,
-  // ssl: { rejectUnauthorized: false }, // if needed for cloud DB
+  ssl: isProduction ? { rejectUnauthorized: false } : undefined,
 });
